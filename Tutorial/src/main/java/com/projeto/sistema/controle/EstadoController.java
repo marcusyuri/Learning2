3d42c2f602f0d3dddfa0947413b74d6683e7e938
Marcus Yúri Maranhão Leal <marcus.yuri@gmail.com>
2024-06-30 12:16:24 -0300

    Aula 5

--- Tutorial/src/main/java/com/projeto/sistema/controle/EstadoController.java
+++ Tutorial/src/main/java/com/projeto/sistema/controle/EstadoController.java
@@ -1,9 +1,12 @@
 package com.projeto.sistema.controle;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,13 +26,35 @@ public class EstadoController {
 		return mv;
 	}
 
+	@GetMapping("/listarEstado")
+	public ModelAndView listar() {
+		ModelAndView mv = new ModelAndView("administrativo/estados/lista");
+		mv.addObject("listaEstados", estadoRepository.findAll());
+		return mv;
+	}
+
+	@GetMapping("/editarEstado/{id}")
+	public ModelAndView editar(@PathVariable("id") Long id) {
+		Optional<Estado> estado = estadoRepository.findById(id);
+
+		return cadastrar(estado.get());
+	}
+	
+	@GetMapping("/removerEstado/{id}")
+	public ModelAndView remover(@PathVariable("id") Long id) {
+		Optional<Estado> estado = estadoRepository.findById(id);
+		estadoRepository.delete(estado.get());
+
+		return listar();
+	}
+
 	@PostMapping("/salvarEstado")
 	public ModelAndView gravarEstado(Estado estado, BindingResult result) {
 		if (result.hasErrors())
 			return cadastrar(estado);
 
 		estadoRepository.saveAndFlush(estado);
-		
+
 		return cadastrar(new Estado());
 	}
 
